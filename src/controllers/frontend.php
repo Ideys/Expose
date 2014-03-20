5e08ad77b08e9250185b2c14c5d53d62b8c7bbca
Valentin Carage <valentin.carage@gmail.com>
2014-03-20 14:25:25 +0100

    s for section

--- src/controllers/frontend.php
+++ src/controllers/frontend.php
@@ -86,12 +86,12 @@ $frontendController->get('/', $frontendContent)
 ->bind('homepage')
 ;
 
-$frontendController->match('/c/{slug}', $frontendContent)
+$frontendController->match('/s/{slug}', $frontendContent)
 ->bind('section')
 ->method('GET|POST')
 ;
 
-$frontendController->match('/c/{slug}/{itemSlug}', $frontendContent)
+$frontendController->match('/s/{slug}/{itemSlug}', $frontendContent)
 ->bind('section_item')
 ->method('GET|POST')
 ;
