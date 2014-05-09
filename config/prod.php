f7db7061fe1352205033f6493165a040efb22e00
Valentin Carage <valentin.carage@gmail.com>
2014-05-09 16:52:50 +0200

    Update foundation style

--- config/prod.php
+++ config/prod.php
@@ -47,4 +47,4 @@ $app['twig.options'] = array('cache' => __DIR__.'/../var/cache/twig');
 $app['languages'] = array('en', 'fr');
 $app['locale_fallback'] = 'fr';
 
-$app['semver'] = '0.13.6';
+$app['semver'] = '0.14';
